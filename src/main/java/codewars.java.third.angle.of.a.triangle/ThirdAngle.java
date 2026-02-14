2e0c063faa87af420bc93bf0f13135a6e240dcaa
whiteburn <erunda995@yandex.ru>
2026-02-14 12:03:45 +0500

    Initial commit

---
+++ src/main/java/codewars.java.third.angle.of.a.triangle/ThirdAngle.java
@@ -0,0 +1,8 @@
+package codewars.java.third.angle.of.a.triangle;
+
+public class ThirdAngle {
+    public static int otherAngle(int angle1, int angle2) {
+        // TODO: Place code here
+        return 180 - angle1 - angle2;
+    }
+}
